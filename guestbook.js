a33a19e1182a8a10382ca1748b2c57fd17eea0a8
noelstepha <stephanie.c.noel@gmail.com>
2021-11-11 16:57:31 +0100

    Update api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9c239cd0.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
